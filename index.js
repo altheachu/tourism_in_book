2d9c3a545327f4c756e70847b628df657c380fa6
cch <aehtla2012@gmail.com>
2024-07-10 23:49:56 +0800

    [IMP] index hover animation

--- index.js
+++ index.js
@@ -6,13 +6,29 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 const API_URL = "https://covers.openlibrary.org/b/isbn/";
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "book",
+  password: "1234",
+  port: 5432,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res) => {
+  let codeNameList = []
+  const result = await db.query("SELECT * FROM countries"); 
+  result.rows.forEach((row) => {
+    let codeNameItem = row.country_code + "|" + row.country_name;
+    codeNameList.push(codeNameItem);
+  });
+  res.render("index.ejs", 
+    {codeNameList: codeNameList}
+  );
 });
 
 app.post("/changeTab", (req, res) => {
@@ -27,6 +43,10 @@ app.post("/changeTab", (req, res) => {
 
 app.get("/note/1", (req, res) => {
   res.render("note.ejs");
+});
+
+app.get("/back", (req, res) => {
+  res.render("thought.ejs");
 });
 
 app.listen(port, () => {
