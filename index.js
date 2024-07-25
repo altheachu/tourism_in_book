343e31e64714ac085f218b3001581f2d382ead52
cch <aehtla2012@gmail.com>
2024-07-25 10:32:15 +0800

    [IMP] imgUrl modified

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 const API_URL = `https://covers.openlibrary.org/b/isbn/`;
+const imgSize = "M";
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
@@ -74,19 +75,19 @@ app.get("/", async (req, res) => {
   );
 });
 
-app.post("/changeTab", async (req, res) => {
+app.get("/changeTab", async (req, res) => {
   try {
-    const currentTab = req.body.tab;
+    const currentTab = req.query.tab;
     if (currentTab === "text"){
       const bookSortOptions = await getBookSortOptions();
       const countryNames = await getCountryNameList();
       const querySql = "select id, book_id, read_date, recommendation, summary, note_id, " + 
-      "amazon_url, isbn, $1||isbn||'-'||$2||'.jpg' as img_url, title, location, " + 
+      "amazon_url, isbn, isbn||'-'||$1||'.jpg' as img_name, title, location, " + 
       "TO_CHAR(read_date, 'YYYY-MM-DD') as read_date_str from records r inner join " +
       "(select b.id as tmp_book_id, b.isbn, b.title, c.country_name || ', ' || b.area as location " +
       "from books b inner join countries c on b.country = c.country_code) t " + 
       "on t.tmp_book_id = r.book_id order by t.title";
-      const result = await db.query(querySql, [API_URL,'M']);
+      const result = await db.query(querySql, [imgSize]);
       const rtnObj = {
         articles: result.rows,
         bookSortOptions: bookSortOptions,
@@ -110,12 +111,12 @@ app.post("/thought", async(req, res) => {
     const countryNames = await getCountryNameList();
     const countryCode = req.body.countryCode;
     const querySql = "select id, book_id, read_date, recommendation, summary, note_id, " + 
-    "amazon_url, isbn, $1||isbn||'-'||$2||'.jpg' as img_url, title, location, " + 
+    "amazon_url, isbn, isbn||'-'||$1||'.jpg' as img_name, title, location, " + 
     "TO_CHAR(read_date, 'YYYY-MM-DD') as read_date_str from records r inner join " +
     "(select b.id as tmp_book_id, b.isbn, b.title, c.country_name || ', ' || b.area as location " +
-    "from books b inner join countries c on b.country = c.country_code where b.country = $3) t " + 
+    "from books b inner join countries c on b.country = c.country_code where b.country = $2) t " + 
     "on t.tmp_book_id = r.book_id order by t.title";
-    const result = await db.query(querySql, [API_URL,'M', countryCode]);
+    const result = await db.query(querySql, [imgSize, countryCode]);
     const rtnObj = {
       articles: result.rows,
       bookSortOptions: bookSortOptions,
@@ -135,25 +136,25 @@ app.post("/searchArticles", async (req, res) => {
   const sort = req.body.sort;
 
   let querySql = "select id, book_id, read_date, recommendation, summary, note_id, " + 
-  "amazon_url, isbn, $1||isbn||'-'||$2||'.jpg' as img_url, title, location, " + 
+  "amazon_url, isbn, isbn||'-'||$1||'.jpg' as img_name, title, location, " + 
   "TO_CHAR(read_date, 'YYYY-MM-DD') as read_date_str from records r inner join " +
   "(select b.id as tmp_book_id, b.isbn, b.title, c.country_name, c.country_name || ', ' || b.area as location " +
   "from books b inner join countries c on b.country = c.country_code ";
-  let sqlParams = [API_URL,'M'];
+  let sqlParams = [imgSize];
 
   if(title || country){
     querySql += "where ";
     
     if(title) {
-      querySql += "b.title like '%'||$3||'%' "
+      querySql += "b.title like '%'||$2||'%' "
       sqlParams.push(title);
     } else {
-      querySql += "1=$3 ";
+      querySql += "1=$2 ";
       sqlParams.push("1");
     }
 
     if(country){
-      querySql += "and c.country_name like '%'||$4||'%' "
+      querySql += "and c.country_name like '%'||$3||'%' "
       sqlParams.push(country);
     }
   }
@@ -185,11 +186,11 @@ app.post("/searchArticles", async (req, res) => {
 
 app.get("/note/:id", async (req, res) => {
   const note_id = req.params.id;
-  const querySql = "select *, $1||isbn||'-'||$2||'.jpg' as img_url from books b inner join " + 
+  const querySql = "select *, isbn||'-'||$1||'.jpg' as img_name from books b inner join " + 
   "(select n.content as note, r.book_id, TO_CHAR(r.read_date, 'YYYY-MM-DD') as read_date_str , " + 
   "r.recommendation, r.summary, r.amazon_url from notes n inner join records r " + 
-  "on n.id = r.note_id where n.id = $3) t on b.id = t.book_id";
-  const result = await db.query(querySql, [API_URL,'M', note_id]);
+  "on n.id = r.note_id where n.id = $2) t on b.id = t.book_id";
+  const result = await db.query(querySql, [imgSize, note_id]);
   let article;
   if(result.rows.length > 0){
     article = result.rows[0];
@@ -205,12 +206,12 @@ app.get("/searchArticles", async (req, res) => {
     const bookSortOptions = await getBookSortOptions();
     const countryNames = await getCountryNameList();
     const querySql = "select id, book_id, read_date, recommendation, summary, note_id, " + 
-    "amazon_url, isbn, $1||isbn||'-'||$2||'.jpg' as img_url, title, location, " + 
+    "amazon_url, isbn, isbn||'-'||$1||'.jpg' as img_name, title, location, " + 
     "TO_CHAR(read_date, 'YYYY-MM-DD') as read_date_str from records r inner join " +
     "(select b.id as tmp_book_id, b.isbn, b.title, c.country_name || ', ' || b.area as location " +
     "from books b inner join countries c on b.country = c.country_code) t " + 
     "on t.tmp_book_id = r.book_id order by t.title";
-    const result = await db.query(querySql, [API_URL,'M']);
+    const result = await db.query(querySql, [imgSize]);
     const rtnObj = {
       articles: result.rows,
       bookSortOptions: bookSortOptions,
@@ -222,26 +223,21 @@ app.get("/searchArticles", async (req, res) => {
   }
 });
 
-app.get("/test", async (req, res) => {
+app.get("/img/:imgName", async (req, res) => {
   try {
-    console.log('test');
+    const imgUrl = `${API_URL}${req.params.imgName}` 
+    // 內部圖片，sendFile可以發送各種類型文件，包含json跟html
     // const imagePath = path.resolve("C:/html_side_project/tourism_in_book", 'public', 'imgs', 'src.jpeg');
     // res.sendFile(imagePath);
-    try {
-      console.log('test');
-      // 下載外部圖片
-      const response = await axios.get("https://sive.rs/images/book/NegotiateAnything.webp", { responseType: 'arraybuffer' });
-      const imageBuffer = Buffer.from(response.data, 'binary');
-      
-      // 設置回應的Content-Type為圖片的MIME類型
-      res.set('Content-Type', response.headers['content-type']);
-      res.send(imageBuffer);
-    } catch (error) {
-      console.log(error);
-      res.status(500).send('Error occurred');
-    }
+    // 下載外部圖片
+    const response = await axios.get(imgUrl, { responseType: 'arraybuffer' });
+    const imageBuffer = Buffer.from(response.data, 'binary');     
+    // 設置回應的Content-Type為圖片的MIME類型
+    res.set('Content-Type', response.headers['content-type']);
+    res.send(imageBuffer);
   } catch (error){
     console.log(error);
+    res.status(500).send('Error occurred');
   }
 });
 
